b457d05f250b3b6597b35692774d96b80bb3cbd2
Francesca Mancini <r03fm14@abdn.ac.uk>
2017-01-18 15:52:22 +0000

    Change loop to include December

--- NewspaperMining.R
+++ NewspaperMining.R
@@ -3,3 +3,26 @@
 #Guardian
 #########################
 library(GuardianR)
+
+key<-"cd31f5ae-cf20-47f6-b6e0-e80b027825ec"
+articles<-NULL
+
+for(y in 1999:2016){
+  for(m in 1:12){
+    if (m!=12){
+    articles_tmp<-get_guardian("conservation+wildlife+nature+biodiversity",from.date=paste(y,"-",m,"-01",sep=""),to.date=paste(y,"-",m+1,"-01",sep=""),section="environment",api.key=key)
+    } else {articles_tmp<-get_guardian("conservation+wildlife+nature+biodiversity",from.date=paste(y,"-",m,"-02",sep=""),to.date=paste(y,"-",m,"-31",sep=""),section="environment",api.key=key)}
+    if(nrow(articles_tmp)!=0) {
+      articles<-rbind(articles,articles_tmp)}
+  }
+}
+
+write.table(articles,"Guardian.txt",sep="/t",row.names=FALSE)
+
+#########################
+#NYTimes
+#########################
+
+
+
+
