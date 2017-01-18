f551f7164c6b1161a9bca588e00ba48a30d41e2e
Francesca Mancini <r03fm14@abdn.ac.uk>
2017-01-18 17:12:06 +0000

    First attempt NYT scraping

--- NewspaperMining.R
+++ NewspaperMining.R
@@ -17,11 +17,63 @@ for(y in 1999:2016){
   }
 }
 
-write.table(articles,"Guardian.txt",sep="/t",row.names=FALSE)
+articles_dupl<-duplicated(articles[,"id"])
+articles_unique<-articles[-which(articles_dupl=="TRUE"),]
+write.table(articles_unique,"Guardian.txt",sep="/t",row.names=FALSE)
 
 #########################
 #NYTimes
 #########################
+library(httr)
+resp <- GET(paste("http://api.nytimes.com/svc/search/v2/articlesearch.json?q=conservation&facet_field=source&facet_filter=true&begin_date=20010101&end_date=20020101&api-key=",NYTkey,sep=""))
+
+print(content(resp, 'parsed')$response$facets)
+
+
+makeURL <- function(q=NULL, fq=NULL, begin_date=NULL, end_date=NULL, key=getOption("nyt_as_key"), page=0, 
+                    sort=NULL, fl=NULL, hl=NULL, facet_field=NULL, facet_filter=NULL){
+  arglist <- list(q=q, fq=fq, begin_date=begin_date, end_date=end_date, 'api-key'=key, page=page,
+                  sort=sort, fl=fl, hl=hl, facet_field=facet_field, facet_filter=facet_filter)
+  url <- 'http://api.nytimes.com/svc/search/v2/articlesearch.json?'
+  for(i in 1:length(arglist)){
+    if(is.null(unlist(arglist[i]))==F){
+      url <- paste0(url, '&', names(arglist[i]), '=', arglist[i])
+    }
+  }
+  return(url)
+}
+
+url1<-makeURL(q="conservation",begin_date="20010101",end_date="20020101",key=NYTkey)
+
+
+getMeta <- function(url, pages=Inf, sleep=0.1, tryn=3) {
+  art <- list()
+  i <- 1
+  e <- seq(-tryn, -tryn/2, length.out=tryn) # initialize list of failed pages with arbitrary negative numbers
+  while(i<=pages){
+    if(length(unique(e[(length(e)-(tryn-1)):length(e)]))==1) i <- i+1 ## attempt tryn times before moving on
+    tryget <- try({
+      urlp <- gsub("page='\d+'", paste0("page=", i), url) ## get the next page
+      p <- GET(urlp) ## actually make GET request
+      pt <- content(p, "parsed") ## retrieve contents of response formatted in a nested R list
+      if(length(pt$response$docs)>0) art <- append(art, pt$response$docs) ## add articles to list
+      else {print(paste0(i, "pages collected")); break}
+      if(i %% 10 ==0) print(paste0(i, "pages of metadata collected"))
+      i <- i+1
+    })
+    
+    ## if there was  a fail...
+    if(class(tryget)=='try-error') {
+      print(paste0(i, ' error - metadata page not scraped'))
+      e <- c(e, i) ## add page i to failed list
+      e <- e[(length(e)-(tryn-1)):length(e)] ## keep the failed list to just tryn elements
+      Sys.sleep(0.5) ## probably scraping too fast -- slowing down
+    }
+    Sys.sleep(sleep)
+  }
+  return(art)
+}
+
 
 
 
